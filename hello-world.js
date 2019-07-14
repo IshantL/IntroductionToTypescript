a47768fee594a8cafa299561c6ad4724112391cf
ishantl <ishantlambhate@gmail.com>
2019-07-14 16:05:06 +0530

    types

---
+++ hello-world.js
@@ -0,0 +1,9 @@
+var fn = function () { return 'response'; };
+//to declare the type
+var a;
+var b;
+var c;
+a = 10;
+b = true;
+c = "hello";
+//a = "hello"(error)
--- hello-world.ts
+++ hello-world.ts
@@ -1,1 +1,13 @@
-var fn = () => 'response'
+var fn = () => 'response';
+
+//to declare the type
+
+var a:number;
+var b:boolean;
+var c: string;
+
+a = 10;
+b = true;
+c = "hello";
+
+//a = "hello"(error)
